cefad6223e756298093d34d3e549b24110daeca4
liyujian3225 <icanflyanywhere@126.com>
2024-10-25 17:54:16 +0800

    主页页面功能修改完善

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,45 +1,21 @@
 import React from 'react'
-import { NavBar, Image, WaterMark } from 'antd-mobile'
+import { Image, Watermark } from "antd";
 import { useNavigate, useRouteProps, Outlet, connect } from 'umi';
-import { SetOutline } from 'antd-mobile-icons'
 import "./index.less"
 
 const Layout = (props) => {
   const routeProps = useRouteProps()
   const { name } = routeProps;
 
-  const right = (
-    <div style={{ fontSize: 24 }} onClick={() => navigate("/setting", { replace: true })}>
-      <SetOutline />
-    </div>
-  )
-
-  const navigate = useNavigate();
-  const goBack = () => {
-    navigate(-1)
-  }
-
   return (
-    <div className="layout" style={{paddingTop: name ? 45 : 0}}>
-      { name ?
-        <NavBar
-          className="navBar"
-          back='返回'
-          right={right}
-          onBack={goBack}>
-          { name }
-        </NavBar> :
-        <></>
-      }
+    <div className="layout">
       <div className="outletContent">
         <Image className="bgImage" src='./image/img_background.png'/>
         <Outlet />
         { (name && !['Selamat datang 欢迎', "附近的人"].includes(name)) ?
           <WaterMark
             content={props.waterMarkContent}
-            gapX={12}
-            gapY={24}
-            fullPage={false}
+            gap={[12, 24]}
           /> :
           <></>
         }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import {
-  Space,
-  Divider,
-  Image,
-
-
-  Mask,
-  SpinLoading,
-
-  Modal
-} from 'antd'
+import { Space, Divider, Image, Spin, Alert } from 'antd'
 import { useNavigate } from 'umi';
 import { extend } from "umi-request";
 import './index.less';
@@ -18,17 +8,11 @@ const request = extend({ timeout: 10000 })
 
 export default () => {
   let navigate = useNavigate();
-  const [visible, setVisible] = useState(true);
-  const [visible_, setVisible_] = useState(false)
+  const [spinning, setSpinning] = React.useState(true);
   const [appLink, setAppLink] = useState("")
 
   useEffect(() => {
-
-    const timer = setTimeout(() => {
-      setVisible(false);
-      setVisible_(true);
-    }, 500);
-
+    const timer = setTimeout(() => { setSpinning(false) }, 500);
     request.get('http://taioassets.oss-cn-beijing.aliyuncs.com/appConfig.json').then(res => {
       const { appLink } = res;
       setAppLink(appLink)
@@ -41,46 +25,17 @@ export default () => {
   }
 
   const downLoadApp = (type) => {
-    let url = "";
-    if(type === "android") {
-      url = appLink
-    }else {
-      url = 'https://apps.apple.com/cn/app/bahasadong/id6502833636'
-    }
-    var user = navigator.userAgent;
-    var isAndroid = user.indexOf("Android") > -1 || user.indexOf("Adr") > -1;
-    var isiOS = !!user.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
-    if(isAndroid) {
-      window.open(url)
-    }else if(isiOS) {
-      window.location.href = url;
-    }
+    let url = type === "android" ? appLink : 'https://apps.apple.com/cn/app/bahasadong/id6502833636';
+    window.open(url, "_blank")
   }
 
   return (
     <div className="home">
-      <Mask opacity='thick' visible={visible}>
-        <div className='overlayContent'>
-          <Space direction='vertical'>
-            <SpinLoading color='primary' />
-            <span>加载中...</span>
-          </Space>
-        </div>
-      </Mask>
-      <Modal
-        visible={visible_}
-        content={<b style={{color: 'red'}}>东东印尼语从未与任何学习机构合作，任何非东东印尼语官方途径购买的账号均无效。东东印尼语将继续和两地司法部门紧密合作，打击盗版课程。</b>}
-        closeOnAction
-        onClose={() => {
-          setVisible_(false)
-        }}
-        actions={[
-          {
-            key: 'confirm',
-            text: '我知道了',
-            primary: true,
-          },
-        ]}
+      <Spin spinning={spinning} fullscreen>加载中...</Spin>
+      <Alert
+        message={<b style={{color: 'red'}}>东东印尼语从未与任何学习机构合作，任何非东东印尼语官方途径购买的账号均无效。东东印尼语将继续和两地司法部门紧密合作，打击盗版课程。</b>}
+        banner
+        closable
       />
       <Image
         className="logoCard"
@@ -98,7 +53,7 @@ export default () => {
         onClick={() => window.open("https://taioassets.oss-cn-beijing.aliyuncs.com/Pdfs/%E4%B8%9C%E4%B8%9C%E5%8D%B0%E5%B0%BC%E8%AF%AD%E7%94%A8%E6%88%B7%E4%BD%BF%E7%94%A8%E6%9C%8D%E5%8A%A1%E5%8D%8F%E8%AE%AE.pdf", "_blank")}
       />
       <Divider>下载东东印尼语App</Divider>
-      <Space className="downLoadBtn" style={{ '--gap': '8px' }} justify="center" block={true}>
+      <Space className="downLoadBtn" size={8} justify="center" block={true}>
         <Image
           className="androidDownload"
           src='./image/Android.png'
